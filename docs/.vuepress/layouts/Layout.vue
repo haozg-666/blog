8a2fd5a09e04810c0e3297e0bcc544614db00fed
haozg-666 <106981170+haozg-666@users.noreply.github.com>
2023-07-16 22:00:33 +0800

    阅读时间完成

--- docs/.vuepress/layouts/Layout.vue
+++ docs/.vuepress/layouts/Layout.vue
@@ -1,7 +1,19 @@
 <script setup lang="ts">
-import { onUnmounted, onMounted, ref } from "vue";
+import { onUnmounted, onMounted, ref, computed } from "vue";
+import { usePageData } from '@vuepress/client';
 import ParentLayout from '@vuepress/theme-default/layouts/Layout.vue'
 import PageToc from "../components/pageToc.vue";
+
+const page = usePageData();
+
+const readingTime = computed(() => {
+  const m = page.value.readingTime.minutes;
+  return m < 1 ? '小于 1 分钟' : `大约 ${Math.floor(m)} 分钟`;
+});
+const showReadingTime = computed(() => {
+  const show = page.value.frontmatter.readingTime;
+  return typeof show === 'undefined' ? true : show;
+});
 
 const isDarkMode = ref(false);
 
@@ -35,6 +47,12 @@ onMounted(() => {
   <ParentLayout :class="{hasGToc}">
     <template #page-content-top>
       <page-toc @toc-length="tocLength" />
+      <ul class="page-info">
+        <li class="page-info-item" v-if="showReadingTime">
+          <label>阅读时间：</label>
+          <span v-text="readingTime"></span>
+        </li>
+      </ul>
     </template>
     <template #page-bottom>
       <comment-service :darkmode="isDarkMode" />
--- docs/js/README.md
+++ docs/js/README.md
@@ -2,6 +2,7 @@
 lang: zh-CN
 title: 哈哈哈
 description: 页面的描述
+readingTime: false
 ---
 
 ### js

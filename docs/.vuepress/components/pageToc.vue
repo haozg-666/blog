641a06ea5f1be8e260defd5a9ebcdd67adcb5b62
haozg-666 <106981170+haozg-666@users.noreply.github.com>
2023-07-17 22:12:35 +0800

    文章导航组件修改

--- docs/.vuepress/components/pageToc.vue
+++ docs/.vuepress/components/pageToc.vue
@@ -1,13 +1,11 @@
 <script setup lang="ts">
-import { defineEmits, computed, toRaw, getCurrentInstance } from "vue";
+import { defineEmits, computed } from "vue";
 import {useRoute} from 'vue-router';
 import { usePageData } from '@vuepress/client';
 
 const emit = defineEmits(['tocLength'])
 
 const route = useRoute();
-console.log(111, toRaw(route))
-console.log(222, getCurrentInstance().proxy.$route)
 const page = usePageData();
 const headers = computed(() => {
   emit('tocLength', page.value.headers);

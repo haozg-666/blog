ba04387dbffcfc010cb47b3cd878e68b869e1923
haozg-666 <106981170+haozg-666@users.noreply.github.com>
2023-07-21 23:36:29 +0800

    首页修改

--- docs/.vuepress/components/Home.vue
+++ docs/.vuepress/components/Home.vue
@@ -1,11 +1,28 @@
 <script setup lang="ts">
+import {computed} from "vue";
+import {
+  ClientOnly,
+  usePageFrontmatter,
+  useSiteLocaleData,
+  withBase,
+} from '@vuepress/client'
+import {DefaultThemeHomePageFrontmatter} from "@vuepress/theme-default/lib/shared";
 
+const frontmatter = usePageFrontmatter<DefaultThemeHomePageFrontmatter>();
+
+const heroImage = computed(() => {
+  return frontmatter.value.heroImage
+})
 </script>
 
 <template>
   <main class="home2">
-    <div class="u-text-c">
-      <img class="github-image" src="https://ghchart.rshah.org/haozg-666" alt="我的 GitHub 贡献图" />
+    <div class="first">
+      <div class="first-inner">
+        <img class="img" :src="heroImage" alt="">
+        <div class="radial">种一棵树最好的时间是十年前<br>其次就是现在</div>
+        <img class="github-image" src="https://ghchart.rshah.org/haozg-666" alt="我的 GitHub 贡献图" />
+      </div>
     </div>
   </main>
 </template>

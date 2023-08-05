54f0aabb1fd97028929deb1b6d5584633f1eb542
haozg-666 <106981170+haozg-666@users.noreply.github.com>
2023-08-05 21:54:30 +0800

    增加自动目录

---
+++ docs/.vuepress/components/AutoCatalog.vue
@@ -0,0 +1,40 @@
+<script setup>
+import SidebarItems from '@vuepress/theme-default/components/SidebarItems.vue'
+</script>
+
+<template>
+  <div class="auto-catalog">
+    <h2>目录</h2>
+    <SidebarItems class="auto-catalog-inner" />
+  </div>
+</template>
+
+<style lang="scss">
+.auto-catalog {
+  .auto-catalog-inner {
+
+    >li:first-child {
+      display: none;
+    }
+
+    .sidebar-item {
+      padding-left: 0;
+      &:not(.sidebar-heading) {
+        color: var(--c-text-accent);
+      }
+    }
+
+    .sidebar-heading {
+      pointer-events: none;
+    }
+
+    .sidebar-item-children {
+      height: auto !important;
+      display: block !important;
+    }
+    .arrow {
+      display: none;
+    }
+  }
+}
+</style>
